ac412064c29e4c0288d365bb5f404ae8742fef61
Tatiana Wansiedler <tatjana@wansiedler.com>
2023-07-21 23:06:15 +0200

    initial commit

---
+++ src/pages/AboulPage/AboutPage.async.tsx
@@ -0,0 +1,6 @@
+import { lazy } from 'react';
+
+export const AboutPageAsync = lazy(() => new Promise((resolve, reject) => {
+    // @ts-ignore
+    setTimeout(()=>resolve(import('./AboutPage')), 1500)
+}));
