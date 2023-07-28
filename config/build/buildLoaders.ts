2798a5cded61000ec3f85135db7743c561d0da8d
Tatiana Wansiedler <tatjana@wansiedler.com>
2023-07-28 12:51:44 +0200

    add babel, add extract plagin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,39 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
-export function buildLoaders({ isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -20,17 +52,21 @@ export function buildLoaders({ isDev}: BuildOptions): webpack.RuleSetRule[] {
 
                 }
             },
-          // Compiles Sass to CSS
-          "sass-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
         ],
     }
-    const typescriptLoader =             {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
     return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
-        cssLoader
+        cssLoader,
     ]
 }
