655df1027900cd45e864618893d12e58c4198e7e
Tatiana Wansiedler <tatjana@wansiedler.com>
2023-08-01 14:17:57 +0200

    added: page loader, not found page, error boundary

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,11 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension':
+            [
+                2,
+                { extensions: ['.js', '.jsx', '.tsx'] },
+            ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -39,7 +43,11 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { code: 100, ignoreComments: true }],
+        'max-len': ['error', { code: 80, ignoreComments: true }],
+        'react/self-closing-comp': ['error', {
+            component: false,
+            html: false,
+        }],
     },
     globals: {
         __IS_DEV__: true,
