5adaed69de130163097b8b863e4c594619182979
Tatiana Wansiedler <tatjana@wansiedler.com>
2023-09-03 17:49:30 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from 'app/App';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import 'shared/config/i18n/i18n';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 render(
     <BrowserRouter>
