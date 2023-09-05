25f842e0d21dd57acfbaec36f142d9e4b6eadf98
Tatiana Wansiedler <tatjana@wansiedler.com>
2023-09-05 16:28:35 +0200

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,15 +2,16 @@ import { fireEvent, screen } from '@testing-library/react';
 import {
     renderWithTranslation,
 } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { ComponentRouter } from 'shared/lib/tests/componentRouter/ComponentRouter';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('test render', () => {
-        renderWithTranslation(<Sidebar />);
+        ComponentRouter(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        ComponentRouter(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
