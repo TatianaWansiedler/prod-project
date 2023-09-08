4741eef091d775b36837071c46456e50e5822772
Tatiana Wansiedler <tatjana@wansiedler.com>
2023-09-08 10:09:49 +0200

    I hate screenshot testing

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Modal } from './Modal';
 
 export default {
     title: 'shared/Modal',
@@ -17,12 +17,12 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo porro culpa similique necessitatibus minus quam iure sapiente quo? Dignissimos cupiditate repellat voluptates. Perspiciatis consequatur aspernatur accusantium debitis pariatur, possimus deserunt eos reprehenderit similique nesciunt soluta delectus quo. Assumenda commodi, fugit nobis culpa ab repellendus ipsa dignissimos facere dolor deserunt esse, nostrum perspiciatis laudantium eos totam sapiente neque sed quis recusandae molestias reiciendis. Minus quisquam dicta et suscipit nobis obcaecati ipsum aut voluptatem eius eveniet eligendi, iusto sit ad similique unde error optio excepturi. Voluptas fugiat nemo facere error cupiditate placeat veritatis doloremque, quam quos temporibus non asperiores minus! Incidunt, molestias.',
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
 
 export const Dark = Template.bind({});
 Dark.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo porro culpa similique necessitatibus minus quam iure sapiente quo? Dignissimos cupiditate repellat voluptates. Perspiciatis consequatur aspernatur accusantium debitis pariatur, possimus deserunt eos reprehenderit similique nesciunt soluta delectus quo. Assumenda commodi, fugit nobis culpa ab repellendus ipsa dignissimos facere dolor deserunt esse, nostrum perspiciatis laudantium eos totam sapiente neque sed quis recusandae molestias reiciendis. Minus quisquam dicta et suscipit nobis obcaecati ipsum aut voluptatem eius eveniet eligendi, iusto sit ad similique unde error optio excepturi. Voluptas fugiat nemo facere error cupiditate placeat veritatis doloremque, quam quos temporibus non asperiores minus! Incidunt, molestias.',
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
 };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
